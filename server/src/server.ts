3a6bf7250818c7011a617a689b585844810112aa
vitorin0o <gvs.vitorino@gmail.com>
2023-01-22 23:31:45 -0300

    primeiro commit

---
+++ server/src/server.ts
@@ -0,0 +1,17 @@
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import { appRoutes } from './lib/routes'
+
+const app = Fastify()
+
+app.register(cors, {
+    origin: true,
+})
+app.register(appRoutes)
+
+app.listen({
+    port: 3333,
+    host:'0.0.0.0',
+}).then((url) => {
+    console.log(`HTTP Server running on ${url}`)
+})
